599242dd1aa3efe927e21aa5e110072c0e85f6aa
xiao liang <joshlx@live.com>
2024-01-13 00:20:31 -0800

    installing imagekit will cause build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import ImageKit from "imagekit";
 
 export default function Home() {
+  const imagekit = new ImageKit({
+    publicKey: process.env.IMAGEKIT_KEY!!,
+    privateKey: process.env.IMAGEKIT_KEY!!,
+    urlEndpoint: "url endpoint",
+  });
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -15,7 +22,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -91,5 +98,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
